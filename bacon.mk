f5c3383861ba8018529ecb67c57ebbcf8ecf4942
Kshitij Gupta <kshitijgm@gmail.com>
2017-02-17 15:53:08 +0530

    bacon: Goodbye SuperSU, hello Magisk

--- bacon.mk
+++ bacon.mk
@@ -17,6 +17,9 @@ ifneq ($(QCPATH),)
 $(call inherit-product-if-exists, $(QCPATH)/common/config/device-vendor.mk)
 endif
 
+# Magisk
+INCLUDE_MAGISK= true
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay vendor/extra/overlays/phone-1080p
 
